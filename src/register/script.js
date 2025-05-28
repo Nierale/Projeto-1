0c0f4abeccce6cd859b10a9a5718b238b1016c1a
Nierale <nith5@hotmail.com>
2025-05-28 01:39:07 -0300

    28-05-2025

--- src/register/script.js
+++ src/register/script.js
@@ -3,13 +3,15 @@ async function wait() {
     window.location.href = "../login/index.html";
 }
 
+// Validação do formulário de cadastro
 function cadastrar() {
     const user = document.getElementById("user").value;
     const password = document.getElementById("password").value;
     const repassword = document.getElementById("re-password").value;
 
-    if (!user || !password || !repassword) {
-        document.getElementById("mensagem").innerText = "Preencha todos os campos.";
+    // Verifica campos vazios
+    if (!user && !password && !repassword) {
+        document.getElementById("mensagem").innerText = "Preencha todos os campos!";
         applyerror("user", "border-styleerror");
         applyerror("luser", "bg-styleerror");
         applyerror("password", "border-styleerror");
@@ -18,9 +20,36 @@ function cadastrar() {
         applyerror("lre-password", "bg-styleerror");
         return;
     }
+    if (!user) {
+        document.getElementById("mensagem").innerText = "Preencha o campo de Usuário!";
+        applyerror("user", "border-styleerror");
+        applyerror("luser", "bg-styleerror");
+        return;
+    }
+    if (user && !password && !repassword) {
+        document.getElementById("mensagem").innerText = "Preencha os campos de Senha!";
+        applyerror("password", "border-styleerror");
+        applyerror("lpassword", "bg-styleerror");
+        applyerror("re-password", "border-styleerror");
+        applyerror("lre-password", "bg-styleerror");
+        return;
+    }
+    if (user && !password && repassword) {
+        document.getElementById("mensagem").innerText = "Preencha o campo de Senha!";
+        applyerror("password", "border-styleerror");
+        applyerror("lpassword", "bg-styleerror");
+        return;
+    }
+    if (!repassword) {
+        document.getElementById("mensagem").innerText = "Preencha o campo de Confirmar senha!";
+        applyerror("re-password", "border-styleerror");
+        applyerror("lre-password", "bg-styleerror");
+        return;
+    }
 
+    // Verifica se a senha e confirmação de senha são iguais
     if (password !== repassword) {
-        document.getElementById("mensagem").innerText = "Digite a mesma senha nos campos."
+        document.getElementById("mensagem").innerText = "Digite a mesma senha nos campos!"
         applyerror("password", "border-styleerror");
         applyerror("lpassword", "bg-styleerror");
         applyerror("re-password", "border-styleerror");
@@ -28,50 +57,34 @@ function cadastrar() {
         return;
     }
 
-    // Salvar no firebase
-    firebase.auth().createUserWithEmailAndPassword(user, password)
-        .then((userCredential) => {
-            document.getElementById("mensagem").innerText = "Cadastro realizado com sucesso!";
-            console.log(userCredential.user);
-            wait();
-        })
-        .catch((error) => {
-            document.getElementById("mensagem").innerText = "Erro: " + error.message;
-        });
+    // Cadastro ok, redirecionamento para Login
+    document.getElementById("mensagem").classList.remove("mensagem");
+    document.getElementById("mensagem").innerText = "Cadastro realizado com sucesso!";
+    wait();
 }
-/*-- Muda o estilo da borda para vermelho --*/
 
+// Muda o estilo da borda para vermelho
 function applyerror(idCampo, Tipo) {
     document.getElementById(idCampo).classList.add(Tipo);
+    document.getElementById("mensagem").classList.add("mensagem");
 }
 
-/*-- Reinicia o estilo da borda para o padrão CSS --*/
-
+//Reinicia o estilo da borda para o padrão CSS
 function removeerror(idCampo, Tipo) {
     document.getElementById(idCampo).classList.remove(Tipo);
     console.log(idCampo, Tipo);
 }
 
-/*["user", "luser", "password", "lpassword", "re-password", "lre-password"].forEach((id) => {
-    const campo = document.getElementById(id);
-    campo.addEventListener("blur", () => {
-        removeerror(id, "border-styleerror");
-        removeerror(label.id, "bg-styleerror");
-    })
-})*/
 ["user", "password", "re-password"].forEach((id) => {
     const input = document.getElementById(id);
     const label = document.querySelector(`label[for="${id}"]`);
-
     input.addEventListener("blur", () => {
         removeerror(id, "border-styleerror");      // remove da borda do input
         removeerror(label.id, "bg-styleerror");  // remove do label (ex: luser)
     });
 });
 
-
-/*----------------------------------------------------*/
-
+// Clicar no icone para mostrar senha
 function show_pass(event) {
     const input = document.getElementById("password");
     const icon = event.target;
@@ -80,7 +93,8 @@ function show_pass(event) {
         input.type = "text";
         icon.classList.remove("bi-key");
         icon.classList.add("bi-eye-slash");
-    } else {
+    }
+    else {
         input.type = "password";
         icon.classList.remove("bi-eye-slash");
         icon.classList.add("bi-key");
@@ -94,7 +108,8 @@ function show_repass(event) {
         input.type = "text";
         icon.classList.remove("bi-key");
         icon.classList.add("bi-eye-slash");
-    } else {
+    }
+    else {
         input.type = "password";
         icon.classList.remove("bi-eye-slash");
         icon.classList.add("bi-key");
